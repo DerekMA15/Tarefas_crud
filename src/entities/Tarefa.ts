49ef4a738949d8782e1b3b2c8229174b6ad3bf8b
DerekMA15 <derekmadev@gmail.com>
2025-12-12 00:54:16 -0300

    primeiro commit

---
+++ src/entities/Tarefa.ts
@@ -0,0 +1,7 @@
+// definição da nossa estutura de dados que nossa API vai receber e enviar.
+
+export interface Tarefa {
+    id: number;
+    titulo: string;
+    concluida:boolean;
+};
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+// testar
+
+import { Tarefa } from './entities/Tarefa';
+
+let tarefas: Tarefa[] = [
+    { id: 1, titulo: "Configurar Ambiente", concluida: true },
+    { id: 2, titulo: "Criar Rota GET", concluida: false }
+
+];
+
+
+console.log(tarefas)
